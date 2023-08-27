6155f5320378c18f565b055efb48a3aa8e7929cc
Kiril Yanev <kl_yanev@abv.bg>
2023-08-27 19:49:42 +0300

    Basic skeleton for Project component

---
+++ src/components/Project/Project.js
@@ -0,0 +1,18 @@
+import './Project.css';
+
+const Project = ({
+
+}) => {
+    return (
+        <div className='project'>
+            <img src="calculator-image.jpg" alt="white-origami" />
+            <div id='project-info'>
+                <a>thisislink</a>
+                <p>This is a simple calculator app</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Project;
